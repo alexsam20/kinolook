883baa1461cd0e6137c889c94c544d3cdccdc599
Alex SaM <alsam3637@gmail.com>
2025-05-14 23:01:05 -0400

    Router class.

--- kernel/Router/Router.php
+++ kernel/Router/Router.php
@@ -4,6 +4,17 @@ namespace App\Kernel\Router;
 
 class Router
 {
+    private array $routes = [
+        'GET' => [],
+        'POST' => [],
+    ];
+
+    public function __construct()
+    {
+        $routes = $this->initRoutes();
+    }
+
+
     public function dispatch(string $uri)
     {
         $routes = $this->getRoutes();
@@ -11,6 +22,19 @@ class Router
         $routes[$uri]();
     }
 
+    private function initRoutes()
+    {
+        $routes = $this->getRoutes();
+
+        foreach ($routes as $route) {
+            $this->routes[$route->getMethod()][$route->getUri()] = $route;
+        }
+//        dd($this->routes);
+    }
+
+    /**
+     * @return Route[]
+     */
     public function getRoutes(): array
     {
         return require APP_PATH.'/config/routes.php';
