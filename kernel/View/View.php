ed5b8024f07e071fb8df80718cdb3bba8b171d7a
Alex SaM <alsam3637@gmail.com>
2025-05-15 11:20:37 -0400

    In the View class, I changed the page method.

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -6,13 +6,20 @@ class View
 {
     public function page(string $name): void
     {
+
+        $viewPath = APP_PATH."/views/pages/$name.php";
+
+        if (! file_exists($viewPath)) {
+            throw new \RuntimeException("View $viewPath does not exist");
+        }
+
         extract(['view' => $this]);
 
-        include_once APP_PATH."/views/pages/{$name}.php";
+        include_once APP_PATH."/views/pages/$name.php";
     }
 
     public function component(string $name): void
     {
-        include_once APP_PATH."/views/components/{$name}.php";
+        include_once APP_PATH."/views/components/$name.php";
     }
 }
