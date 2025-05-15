5439c87ebfa82903f7d4da87184d490821ca90e2
Alex SaM <alsam3637@gmail.com>
2025-05-15 09:01:28 -0400

    Create Request class.

--- kernel/App.php
+++ kernel/App.php
@@ -2,6 +2,7 @@
 
 namespace App\Kernel;
 
+use App\Kernel\Http\Request;
 use App\Kernel\Router\Router;
 
 class App
@@ -9,10 +10,8 @@ class App
     public function run(): void
     {
         $router = new Router;
+        $request = Request::createFromGlobals();
 
-        $uri = $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-
-        $router->dispatch($uri, $method);
+        $router->dispatch($request->uri(), $request->method());
     }
 }
