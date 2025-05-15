8d1e1469a3291155e316e8776251781938de7342
Alex SaM <alsam3637@gmail.com>
2025-05-15 10:00:10 -0400

    Create abstract Controller class.

--- src/Controllers/MovieController.php
+++ src/Controllers/MovieController.php
@@ -2,14 +2,13 @@
 
 namespace App\Controllers;
 
+use App\Kernel\Controller\Controller;
 use App\Kernel\View\View;
 
-class MovieController
+class MovieController extends Controller
 {
     public function index(): void
     {
-        $view = new View;
-
-        $view->page('movie');
+        $this->view('movies');
     }
 }
