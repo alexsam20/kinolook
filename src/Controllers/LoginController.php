74c16a6d6f8afcc640dda42b82b297a12bf0efbd
Alex SaM <alsam3637@gmail.com>
2025-06-10 18:43:56 -0400

    Create User class

--- src/Controllers/LoginController.php
+++ src/Controllers/LoginController.php
@@ -15,15 +15,15 @@ class LoginController extends Controller
     {
         $email = $this->request()->input('email');
         $password = $this->request()->input('password');
-        //dd($this->auth()->attempt($email, $password), $_SESSION);
+//        dd($this->auth()->attempt($email, $password), $_SESSION);
 
-        /*if ($this->auth()->attempt($email, $password)) {
+        if ($this->auth()->attempt($email, $password)) {
             $this->redirect('/');
         }
 
         $this->session()->set('error', 'Неверный логин или пароль');
 
-        $this->redirect('/login');*/
+        $this->redirect('/login');
     }
 
     /*public function logout(): void
--- views/components/header.php
+++ views/components/header.php
@@ -1,8 +1,9 @@
 <?php /** @var \Kernel\Auth\AuthInterface $auth */ ?>
-<?php $user = $auth->user(); ?>
+<?php $user = $auth->user();
+//dump($user->email()); ?>
 <header>
     <?php if ($auth->check()): ?>
-        <h3>User: <?php echo $user['email'] ?></h3>
+        <h3>User: <?php echo $user->email(); ?></h3>
         <button>Logout</button>
         <hr />
     <?php endif; ?>
