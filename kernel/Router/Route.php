91619679cf7119178c59921f51b9f45ea4a5bebd
Alex SaM <alsam3637@gmail.com>
2025-06-10 19:26:16 -0400

    Add AuthMiddleware in the Route class

--- kernel/Router/Route.php
+++ kernel/Router/Route.php
@@ -7,17 +7,18 @@ class Route
     public function __construct(
         private readonly string $uri,
         private readonly string $method,
-        private                 $action
+        private                 $action,
+        private readonly array $middleware = []
     ) {}
 
-    public static function get(string $uri, $action): static
+    public static function get(string $uri, $action, array $middleware = []): static
     {
-        return new static($uri, 'GET', $action);
+        return new static($uri, 'GET', $action, $middleware);
     }
 
-    public static function post(string $uri, $action): static
+    public static function post(string $uri, $action, array $middleware = []): static
     {
-        return new static($uri, 'POST', $action);
+        return new static($uri, 'POST', $action, $middleware);
     }
 
     public function getUri(): string
@@ -33,5 +34,15 @@ class Route
     public function getAction(): mixed
     {
         return $this->action;
+    }
+
+    public function hasMiddleware(): bool
+    {
+        return !empty($this->middleware);
+    }
+
+    public function getMiddleware(): array
+    {
+        return $this->middleware;
     }
 }
