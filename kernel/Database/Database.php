b03b3728f756fd57d18fdf71102440889f044c43
Alex SaM <alsam3637@gmail.com>
2025-06-10 13:40:07 -0400

    Implementation of the first method in the Database class

--- kernel/Database/Database.php
+++ kernel/Database/Database.php
@@ -26,8 +26,6 @@ class Database implements DatabaseInterface
 
         $stmt = $this->pdo->prepare($sql);
 
-//        dd($stmt);
-
         try {
             $stmt->execute($data);
         } catch (PDOException $exception) {
@@ -35,6 +33,25 @@ class Database implements DatabaseInterface
         }
 
         return (int) $this->pdo->lastInsertId();
+    }
+
+    public function first(string $table, array $conditions = []): ?array
+    {
+        $where = '';
+
+        if (count($conditions) > 0) {
+            $where = 'WHERE '.implode(' AND ', array_map(fn ($field) => "$field = :$field", array_keys($conditions)));
+        }
+
+        $sql = "SELECT * FROM $table $where LIMIT 1";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute($conditions);
+
+        $result = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        return $result ?: null;
     }
 
     private function connect(): void
--- kernel/Database/DatabaseInterface.php
+++ kernel/Database/DatabaseInterface.php
@@ -6,9 +6,9 @@ interface DatabaseInterface
 {
     public function insert(string $table, array $data): int|false;
 
-    /*public function first(string $table, array $conditions = []): ?array;
+    public function first(string $table, array $conditions = []): ?array;
 
-    public function get(string $table, array $conditions = [], array $order = [], int $limit = -1): array;
+    /*public function get(string $table, array $conditions = [], array $order = [], int $limit = -1): array;
 
     public function delete(string $table, array $conditions = []): void;
 
