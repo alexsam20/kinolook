9d121e8d1a1668ba634d61e58a1abf02581ff310
Alex SaM <alsam3637@gmail.com>
2025-06-09 22:41:43 -0400

    Create AuthInterface and Auth classes

---
+++ kernel/Auth/Auth.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kernel\Auth;
+
+use Kernel\Auth\AuthInterface;
+
+class Auth implements AuthInterface
+{
+    public function __construct(
+        private DatabaseInterface $db,
+        private SessionInterface $session
+    )
+    {
+    }
+
+    public function attempt(string $username, string $password): bool
+    {
+        // TODO: Implement attempt() method.
+    }
+
+    public function logout(): void
+    {
+        // TODO: Implement logout() method.
+    }
+
+    public function check(): bool
+    {
+        // TODO: Implement check() method.
+    }
+
+    public function user(): ?array
+    {
+        // TODO: Implement user() method.
+    }
+}
---
+++ kernel/Auth/AuthInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Kernel\Auth;
+
+interface AuthInterface
+{
+    public function attempt(string $username, string $password): bool;
+
+    public function logout(): void;
+
+    public function check(): bool;
+
+    public function user(): ?array;
+
+    /*public function id(): ?int;
+
+    public function table(): string;
+
+    public function username(): string;
+
+    public function password(): string;
+
+    public function sessionField(): string;*/
+}
