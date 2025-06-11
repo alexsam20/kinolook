066dae79f29bbc78f59f4c723923842611cbdc7b
Alex SaM <alsam3637@gmail.com>
2025-06-10 20:13:35 -0400

    Create UploadedFileInterface and UploadedFile classes

---
+++ kernel/Upload/UploadedFile.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Kernel\Upload;
+
+use Kernel\Upload\UploadedFileInterface;
+
+class UploadedFile implements UploadedFileInterface
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $type,
+        public readonly string $tmpName,
+        public readonly int $error,
+        public readonly int $size,
+    ) {
+    }
+
+    public function move(string $path): bool
+    {
+        // TODO: Implement move() method.
+    }
+}
